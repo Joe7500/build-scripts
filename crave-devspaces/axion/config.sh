d683569a4fb8a3d5123d5c8075b8b157ec9e79e8
Joe7500 <j7545032@gmail.com>
2025-05-08 14:22:03 +0200

    update

---
+++ crave-devspaces/axion/config.sh
@@ -0,0 +1,7 @@
+
+export PACKAGE_NAME="axion"
+export PACKAGE_MANIFEST_ARGS="-u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs"
+export CRAVE_MANIFEST_ARGS="-u https://github.com/accupara/los22.git -b lineage-22.1 --git-lfs"
+export CRAVE_SCRIPT="axion.sh"
+export CRAVE_YAML="../../etc/secrets/crave-lineage-22.yaml"
+
