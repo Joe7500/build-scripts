73d9fc2b2bb75df794229e5e9925459dd0399fe4
Joe7500 <62238620+Joe7500@users.noreply.github.com>
2025-05-10 19:03:43 +0200

    Update build-auto.sh

--- scripts/build-auto.sh
+++ scripts/build-auto.sh
@@ -11,11 +11,9 @@ source etc/config.sh
 if [ $? -ne 0 ]; then echo failed to source config; exit 1 ; fi
 cd -
 
-if [ -f $LOCK_FILE ]; then
-        if [ $? -eq 0 ]; then
-                echo build-auto is already running
-                exit 1
-        fi
+if ls $LOCK_FILE; then
+        echo build-auto is already running
+        exit 1
 fi
 echo $$ > $LOCK_FILE
 
@@ -25,7 +23,7 @@ cd $TEST_UPDATES_ROOT
 
 for i in crDroidAndroid-14 crDroidAndroid-15 lineage-21 lineage-20 lineage-22 RisingOS ; do
 	cd $TEST_UPDATES_ROOT
-	if ls $REMOTE_BUSY_LOCK  ; then echo "skipping $i" ; continue ; fi
+	if ls $REMOTE_BUSY_LOCK ; then echo "skipping $i" ; continue ; fi
 	bash $i.sh
 	if [ $? -eq 0 ]; then
 		if ! ls $REMOTE_BUSY_LOCK; then
