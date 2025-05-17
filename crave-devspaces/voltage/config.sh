1028bef0175b6bbd66d1bee38c3e70fe97bea583
Joe7500 <62238620+Joe7500@users.noreply.github.com>
2025-05-17 15:42:56 +0200

    Update config.sh

--- crave-devspaces/voltage/config.sh
+++ crave-devspaces/voltage/config.sh
@@ -1,6 +1,6 @@
 
 export PACKAGE_NAME="voltage"
-export PACKAGE_MANIFEST_ARGS="-u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfss"
+export PACKAGE_MANIFEST_ARGS="-u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfs"
 export CRAVE_MANIFEST_ARGS="-u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs"
 export CRAVE_SCRIPT="voltage.sh"
 export CRAVE_YAML="../../etc/secrets/crave-lineage-22.yaml"
