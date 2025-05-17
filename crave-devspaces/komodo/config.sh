9edfb438fe635f6f7a3075b7872a4fab113253cf
Joe7500 <j7545032@gmail.com>
2025-05-17 15:39:05 +0200

    add voltage

---
+++ crave-devspaces/komodo/config.sh
@@ -0,0 +1,10 @@
+
+export PACKAGE_NAME="KomodoOS"
+export PACKAGE_MANIFEST_ARGS="-u https://github.com/Komodo-OS/manifest -b 15 --git-lfs"
+export CRAVE_MANIFEST_ARGS="-u https://github.com/accupara/los22.git -b lineage-22.1 --git-lfs"
+export CRAVE_SCRIPT="komodo.sh"
+export CRAVE_YAML="../../etc/secrets/crave-lineage-22.yaml"
+
+
+
+
