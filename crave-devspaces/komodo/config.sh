b2334868a3d700abaa44a8e2740b414a0cd64aad
Joe7500 <j7545032@gmail.com>
2025-05-20 20:35:57 +0200

    update

--- crave-devspaces/komodo/config.sh
+++ crave-devspaces/komodo/config.sh
@@ -1,9 +1,9 @@
 
 export PACKAGE_NAME="KomodoOS"
 export PACKAGE_MANIFEST_ARGS="-u https://github.com/Komodo-OS/manifest -b 15 --git-lfs"
-export CRAVE_MANIFEST_ARGS="-u https://github.com/accupara/los22.git -b lineage-22.1 --git-lfs"
+export CRAVE_MANIFEST_ARGS="-u https://android.googlesource.com/platform/manifest -b master --git-lfs"
 export CRAVE_SCRIPT="komodo.sh"
-export CRAVE_YAML="../../etc/secrets/crave-lineage-22.yaml"
+export CRAVE_YAML="../../etc/secrets/crave-aosp.yaml"
 
 
 
