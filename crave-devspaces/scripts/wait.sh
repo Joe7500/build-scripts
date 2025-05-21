637e71e90af2a0e915fd557935fdd9d921c6fc31
Joe7500 <j7545032@gmail.com>
2025-05-21 20:49:42 +0200

    update

--- crave-devspaces/scripts/wait.sh
+++ crave-devspaces/scripts/wait.sh
@@ -1,22 +1,40 @@
 
-PACKAGE_NAME=$1
-
 source ../../etc/config.sh
 
 while true; do
    sleep `shuf -n 1 -i 400-900`
 #   sleep 20
-
+#   sleep 5
    if ls $LOCK_FILE; then
+        echo locked
 	continue
    fi
 
    if ls $REMOTE_BUSY_LOCK; then
+	echo remote locked
 	continue
    fi
 
+   CRAVE_LIST=`crave list`
+
+   if ! echo "$CRAVE_LIST" | grep "android.googlesource.com/platform/manifest" ; then
+	echo -e "\\a" ; sleep 1 ; echo -e "\\a";echo -e "\\a" ; sleep 1 ; echo -e "\\a"
+	echo crave list not found. trying again later
+        continue
+   else
+	echo crave list found.
+   fi
+
+   if echo "$CRAVE_LIST" | grep queued ; then
+	echo already queued on crave. locking
+        touch $REMOTE_BUSY_LOCK
+        continue
+   fi
+
+   echo no lock found. starting
    touch $REMOTE_BUSY_LOCK
-   screen -dmS begin-$PACKAGE_NAME bash begin.sh
+   screen -dmS begin-wait bash begin.sh
+   echo -e "\\a" ; sleep 1 ; echo -e "\\a";echo -e "\\a" ; sleep 1 ; echo -e "\\a"
    exit 0
 done
 
