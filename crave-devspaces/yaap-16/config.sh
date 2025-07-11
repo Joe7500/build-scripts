9894599eb704461e85281724965e9cbeb5ee39ef
Joe7500 <j7545032@gmail.com>
2025-07-11 15:44:29 +0200

    update

--- crave-devspaces/yaap-16/config.sh
+++ crave-devspaces/yaap-16/config.sh
@@ -2,5 +2,5 @@
 export PACKAGE_NAME="YAAP-16"
 export PACKAGE_MANIFEST_ARGS="-u https://github.com/yaap/manifest.git -b sixteen --git-lfs"
 export CRAVE_MANIFEST_ARGS="-u https://android.googlesource.com/platform/manifest -b master --git-lfs"
-export CRAVE_SCRIPT="yaap.sh-16"
+export CRAVE_SCRIPT="yaap-16.sh"
 export CRAVE_YAML="../../etc/secrets/crave-yaap.yaml"
