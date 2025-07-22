e9c8ed3e53bdff7b77f0563f2bf699520f198a30
Joe7500 <j7545032@gmail.com>
2025-07-22 21:36:32 +0000

    update

---
+++ crave-devspaces/infinity-x-16/config.sh
@@ -0,0 +1,6 @@
+
+export PACKAGE_NAME="Project_Infinity-X-3"
+export PACKAGE_MANIFEST_ARGS="--depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault"
+export CRAVE_MANIFEST_ARGS="-u https://android.googlesource.com/platform/manifest -b master --git-lfs"
+export CRAVE_SCRIPT="infinity-x-16.sh"
+export CRAVE_YAML="../../etc/secrets/crave-yaap.yaml"
