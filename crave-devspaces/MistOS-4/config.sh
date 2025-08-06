896b0b0182c9fca2fb70c5e9c300584afdf7b9f0
Joe7500 <j7545032@gmail.com>
2025-08-06 13:38:30 +0000

    update

---
+++ crave-devspaces/MistOS-4/config.sh
@@ -0,0 +1,10 @@
+
+export PACKAGE_NAME="MistOS-4"
+export PACKAGE_MANIFEST_ARGS="-u https://github.com/Project-Mist-OS/manifest.git -b 16 --git-lfs"
+export CRAVE_MANIFEST_ARGS="-u https://github.com/accupara/los22.git -b lineage-22.1 --git-lfs"
+export CRAVE_SCRIPT="MistOS-4.sh"
+export CRAVE_YAML="../../etc/secrets/crave-lineage-22.yaml"
+
+
+
+
