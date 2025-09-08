a71727acdb5d4f14fe66f7f079da8601ae0844e4
Joe7500 <j7545032@gmail.com>
2025-09-08 15:46:59 +0000

    update

---
+++ crave-devspaces/crDroidAndroid-16/config.sh
@@ -0,0 +1,7 @@
+
+export PACKAGE_NAME="crDroidAndroid-16"
+export PACKAGE_MANIFEST_ARGS="-u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs"
+export CRAVE_MANIFEST_ARGS="-u https://github.com/accupara/los22.git -b lineage-22.1 --git-lfs"
+export CRAVE_SCRIPT="crdroid-16.sh"
+export CRAVE_YAML="../../etc/secrets/crave-lineage-22.yaml"
+
