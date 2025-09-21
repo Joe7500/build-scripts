f89587379ef368c1ad2fab47d9a18cecf92be665
Joe7500 <j7545032@gmail.com>
2025-09-21 15:03:09 +0200

    update

--- test-updates/lineage-21.sh
+++ test-updates/lineage-21.sh
@@ -2,6 +2,8 @@
 # git clone https://github.com/LineageOS/android_build build
 # git clone https://github.com/LineageOS/android_build_release build-release
 
+UPDATE=0
+
 cd lineage
 cd build
 git switch lineage-21.0
@@ -30,10 +32,49 @@ echo LAST_RELEASE $LAST_RELEASE
 
 if [ "$BUILD_RELEASE" == "$LAST_RELEASE" ]; then
    echo not update
-   exit 1
 else
    echo update
-   if echo "$@" | grep update ; then  echo "$BUILD_RELEASE" > ../LAST_RELEASE_lineage-21; fi
+   UPDATE=1
+fi
+
+LAST_COMMIT=$(cat ../LAST_COMMIT_lineage-21 | head -1)
+echo LAST $LAST_COMMIT
+
+cd android
+git switch lineage-21.0
+if [ $? -ne 0 ]; then echo git switch failed ; exit 1 ; fi
+git pull --rebase
+if [ $? -ne 0 ]; then echo git pull failed ; exit 1 ; fi
+
+CURRENT_COMMIT=$(git log --format=format:%H | head -1)
+echo CURRENT $CURRENT_COMMIT
+
+if [ "$CURRENT_COMMIT" == "$LAST_COMMIT" ]; then
+   echo not update
+else
+   NEW_COMMITS=$(git log --format=format:%H | head -9)
+   for COMMIT in $NEW_COMMITS; do
+      if [ "$COMMIT" == "$LAST_COMMIT" ]; then
+         echo found last commit
+         break
+      else
+         git show -q $COMMIT > ../../commit_msg.txt
+         cat ../../commit_msg.txt | grep -iE 'quarter|security|asb|cve|qpr'
+         if [ $? -eq 0 ]; then
+            echo update
+            UPDATE=1
+            break
+         fi
+      fi
+   done
+fi
+cd -
+
+if echo "$@" | grep update; then echo $CURRENT_COMMIT >../LAST_COMMIT_lineage-21; fi
+if echo "$@" | grep update; then  echo "$BUILD_RELEASE" > ../LAST_RELEASE_lineage-21; fi
+
+if [ $UPDATE -eq 1 ]; then
+   echo update
    exit 0
 fi
 
