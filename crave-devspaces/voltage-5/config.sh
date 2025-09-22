f1b5c37cd729044ced833afac6094575bd86ff3b
Joe7500 <j7545032@gmail.com>
2025-09-22 10:06:28 +0200

    update

--- crave-devspaces/voltage-5/config.sh
+++ crave-devspaces/voltage-5/config.sh
@@ -1,5 +1,5 @@
 
-export PACKAGE_NAME="voltage"
+export PACKAGE_NAME="voltage-5"
 export PACKAGE_MANIFEST_ARGS="-u https://github.com/VoltageOS/manifest.git -b 16 --git-lfs"
 export CRAVE_MANIFEST_ARGS="-u https://github.com/accupara/los22.git -b lineage-22.1 --git-lfs"
 export CRAVE_SCRIPT="voltage-5.sh"
