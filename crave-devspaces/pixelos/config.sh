ffd904fc3ec32b007846302d2c328fc5e1ae5c4a
Joe7500 <j7545032@gmail.com>
2026-01-02 09:12:54 +0000

    update

--- crave-devspaces/pixelos/config.sh
+++ crave-devspaces/pixelos/config.sh
@@ -1,6 +1,6 @@
 
 export PACKAGE_NAME="PixelOS"
-export PACKAGE_MANIFEST_ARGS=" -u https://github.com/PixelOS-AOSP/android_manifest -b sixteen --git-lfs "
+export PACKAGE_MANIFEST_ARGS=" -u https://github.com/PixelOS-AOSP/android_manifest -b sixteen --git-lfs --depth=1 "
 export CRAVE_MANIFEST_ARGS=" -u https://github.com/PixelOS-AOSP/manifest -b fifteen --git-lfs"
 export CRAVE_SCRIPT="pixelos.sh"
 export CRAVE_YAML="../../etc/secrets/crave-pixel.yaml"
